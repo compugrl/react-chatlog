8f3c545d6cf7b65ce2d7183350ca4b061fb51f81
Shayla Logan <shayla.logan709@gmail.com>
2022-06-14 16:38:19 -0700

    Coded Wave 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './App.css';
-import chatMessages from './data/messages.json';
+// import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const message = {
+    sender: 'Vladimir',
+    body: 'why are you arguing with me',
+    timeStamp: '2018-05-29T22:49:06+00:00',
+  };
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>
+          Chat Between {message.sender} and {message.body}
+        </h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry message={message}></ChatEntry>
       </main>
     </div>
   );
